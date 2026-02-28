2241dd4fad033a89a4fea47eac4c95d33c3df8f9
ashishmau <ashishmau@hcl.com>
2026-02-28 20:17:31 +0530

    fix: updated base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://aibgremover-backend.onrender.com";
 
 export default function App() {
   const [file, setFile] = useState(null);
